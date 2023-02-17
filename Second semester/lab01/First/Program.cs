505ed18dd1906494385016de0a37aaeac862bab8
stas <79771234+ninahur1989@users.noreply.github.com>
2023-02-17 10:57:05 +0200

    lab1

---
+++ Second semester/lab01/First/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace First
+{
+    internal class Program
+    {
+        delegate float Operation(float x, float y);
+        static void Main(string[] args)
+        {
+
+            Operation add = (a, b) => a + b;
+            Operation subtract = (a, b) => a - b;
+            Operation multiply = (a, b) => a * b;
+            Operation divide = (a, b) =>
+            {
+                if (b == 0)
+                {
+                    Console.WriteLine("Error: Cannot divide by zero.");
+                    return 0;
+                }
+                return a / b;
+            };
+
+            Console.WriteLine("Enter an operator (+, -, *, /): ");
+            string op = Console.ReadLine();
+
+            Console.WriteLine("Enter two numbers: ");
+            float num1 = float.Parse(Console.ReadLine());
+            float num2 = float.Parse(Console.ReadLine());
+
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine(add(num1, num2));
+                    break;
+                case "-":
+                    Console.WriteLine(subtract(num1, num2));
+                    break;
+                case "*":
+                    Console.WriteLine(multiply(num1, num2));
+                    break;
+                case "/":
+                    Console.WriteLine(divide(num1, num2));
+                    break;
+                default:
+                    Console.WriteLine("Error: Invalid operator.");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Second semester/lab01/Second/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Second
+{
+    delegate int Delegate();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Delegate[] delegates = new Delegate[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                delegates[i] = delegate
+                {
+                    Random rnd = new Random();
+                    return rnd.Next(1, 100);
+                };
+            }
+
+            Func<Delegate[], double> average = delegate (Delegate[] arr)
+            {
+                double sum = 0;
+                foreach (Delegate del in arr)
+                {
+                    sum += del();
+                }
+                return sum / arr.Length;
+            };
+
+            Console.WriteLine("The average value is: " + average(delegates));
+
+            Console.ReadLine();
+        }
+    }
+}
